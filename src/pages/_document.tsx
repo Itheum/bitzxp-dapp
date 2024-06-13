dddbfa49c0a46cab93be622298fe7c65579101aa
Damian <damian.ovidiu27@gmail.com>
2024-06-13 15:57:42 +0300

    feat: hide api key

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,6 @@ class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="/favicon.png" />
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta
             name="description"
             content="Explore Itheum's Bitz XP application on Solana network."
