386fcd040ebc4521377794fe549424a39645c1c8
Damian <damian.ovidiu27@gmail.com>
2024-06-26 21:54:54 +0300

    fix: google tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { AppBar } from '../components/AppBar';
 import { ContentContainer } from '../components/ContentContainer';
 import { Footer } from '../components/Footer';
 import Notifications from '../components/Notification';
+import { GoogleTagManager } from '@next/third-parties/google';
 require('@solana/wallet-adapter-react-ui/styles.css');
 require('../styles/globals.css');
 require('../styles/GetBitz.css');
@@ -15,11 +16,12 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>Itheum Bitz XP</title>
+        <title>Itheum Bitz XPP</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <ContextProvider>
+        <GoogleTagManager gtmId="G-R4JT4ML00N" />
         <div className="flex flex-col flex-auto min-h-[100dvh]">
           <Notifications />
           <AppBar />
